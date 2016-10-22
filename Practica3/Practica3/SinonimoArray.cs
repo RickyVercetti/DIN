d766585e2bae3434a66294368f4eee0fcd277218
juanf <juanf@DESKTOP-S8D6AE1>
2016-10-22 11:27:05 +0200

    Subiendo primera morralla

---
+++ Practica3/Practica3/SinonimoArray.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica3
+{
+    class SinonimoArray
+    {
+
+        private String[][] arraySinonimos;
+
+        public SinonimoArray()
+        {
+            arraySinonimos = new String[0][];
+        }
+        
+
+        public void add(params String[] nuevoSinonimos)
+        {
+
+            foreach(String nuevoSinonimo in nuevoSinonimos)
+            {
+
+                foreach(String[] palabra in arraySinonimos)
+                {
+
+                    if(Array.IndexOf(palabra, nuevoSinonimo) < 0)
+                    {
+                        Array.Resize(ref arraySinonimos, arraySinonimos.Length + 1);
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("La palabra " + palabra + " ya está introducida.");
+                    }
+                }
+            }
+
+        }
+        public void search(String texto)
+        {
+
+        }
+    }
+}
